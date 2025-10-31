8cd65af9f6fbccc18da591c96e9304aa9b7bd071
rashuTomar <rashutomar01@gmail.com>
2025-10-31 14:38:45 +0530

    building first component in react

---
+++ react.js/frontend/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Card.css'
+
+function Card() {
+  return (
+    <div id='card'>
+      <img src="" alt=""  width={100} height={100}/>
+      <h1>ABES Engineering college</h1>
+      <h1>BTech</h1>
+    </div>
+  )
+}
+
+export default Card
